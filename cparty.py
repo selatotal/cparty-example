298bb3fc1952338af01c416d13bd79e414edd70a
Tales Viegas <selatotal@gmail.com>
2018-01-30 11:02:56 -0200

    Finalização do CRUD

--- cparty.py
+++ cparty.py
@@ -20,13 +20,38 @@ def home():
     books = Book.query.all()
     return render_template('home.html', books=books)
 
-@app.route('/', methods=["POST"])
-def addUser():
+@app.route('/book', methods=["GET"])
+def indexBook():
+    return render_template('book.html', book=Book(), submitText='Add')
+
+@app.route('/book/', methods=["POST"])
+def addBook():
     if request.form:
         book = Book(title=request.form['title'], author=request.form['author'])
         db.session.add(book)
         db.session.commit()
     return redirect('/')
+
+@app.route('/book/<int:book_id>', methods=["GET"])
+def getBook(book_id):
+    book = Book.query.get(book_id)
+    return render_template('book.html', book=book, submitText='Update')
+
+@app.route('/book/<int:book_id>', methods=["POST"])
+def updateBook(book_id):
+    book = Book.query.get(book_id)
+    book.title = request.form['title']
+    book.author = request.form['author']
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/book/<int:book_id>/delete', methods=["GET"])
+def deleteBook(book_id):
+    book = Book.query.get(book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect('/')
+
 
 if __name__ == '__main__':
     db.create_all()
